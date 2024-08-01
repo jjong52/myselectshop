7a2e325d77ad9495b98354f6877201014f949420
suin <suin505@gmail.com>
2024-08-01 13:34:59 +0900

    폴더별 관심 상품 조회 구현

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -58,4 +58,24 @@ public class ProductController {
         productService.addFolder(productId, folderId, userDetails.getUser());
 
     }
+
+    // 폴더별 관심 상품 조회
+    @GetMapping("/folders/{folderId}/products")
+    public Page<ProductResponseDto> getProductsInFolder (
+            @PathVariable Long folderId,
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        return productService.getProductsInFolder(
+                folderId,
+                page-1,
+                size,
+                sortBy,
+                isAsc,
+                userDetails.getUser()
+        );
+    }
 }
